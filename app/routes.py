0ffe1988e36ccbde48c2cf528191e8d0f1e4d41f
Oskar-Wulff <oskar9899@gmail.com>
2019-09-05 09:41:12 +0200

    kinda works

--- app/routes.py
+++ app/routes.py
@@ -1,29 +1,18 @@
-from flask import (
-    Flask,
-    render_template,
-    Blueprint, 
-    request, 
-    url_for, 
-    session, 
-    redirect, 
-    json, 
-    jsonify)
+from flask import Flask,render_template,Blueprint, request, url_for, session, redirect, json, jsonify
+import requests
 
 page = Blueprint("page", __name__, template_folder="templates")
 
 users = [
     {
-        "id": 1,
         "username": "Oskar",
         "message": "Blah Blah Blah"
     },
     {
-        "id": 2,
         "username": "Test1",
         "message": "Blah Blah Blah"
     },
     {
-        "id": 3,
         "username": "Test2",
         "message": "Blah Blah Blah"
     }
@@ -41,11 +30,25 @@ def get_users():
     return jsonify({"users": users})
 
 # GET - One user
-@page.route("/api/users/<int:id>")
+@page.route("/api/users/<id>")
 def get_one_user(id):
     for user in users:
-        if user["id"] == id:
+        if user["username"] == id:
             return jsonify({"user": user})
 
     return jsonify({"error": "No user found" }), 400
 
+# POST - Create user
+@page.route("/api/users", methods=["POST"])
+def create_user():
+    users.append({ "username": request.form["username"], "message": request.form["message"] })
+    return redirect(url_for("page.index"))
+
+
+# Vulnerable endpoint
+@page.route("/vuln-endpoint")
+def vuln_endopoint():
+    api_url = requests.get("http://localhost:5000/api/users")
+    data = json.loads(api_url.text)
+
+    return render_template("views/vuln-endpoint.html", data=data)
